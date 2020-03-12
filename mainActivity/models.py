b0812b7024d6f8932d8fcbecbe1dea8b95b853c8
Manab <46393531+manabsaha@users.noreply.github.com>
2020-03-12 23:06:46 +0530

    cust db

--- mainActivity/models.py
+++ mainActivity/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class customer (models.Model):
     cust_name=models.CharField(max_length=255)
+    cust_address=models.CharField(max_length=255)
+    cust_phone=models.CharField(max_length=15)
     cust_type=models.CharField(max_length=255)
+    cust_id=models.CharField(max_length=30)
     cust_since=models.DateField()
 
     def __str__(self):
