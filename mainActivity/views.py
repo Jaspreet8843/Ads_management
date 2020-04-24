ca481804d3ff01db5468d30234e2ae08089cdcb8
Manab <46393531+manabsaha@users.noreply.github.com>
2020-04-24 14:36:28 +0530

    active tab

--- mainActivity/views.py
+++ mainActivity/views.py
@@ -11,8 +11,7 @@ import time
 def index(request):
     # c=customer.objects.filter(cust_type="govt")
     # p=prices(cust_type=c[0], price='10000')
-
-    return render(request, 'localsdirectory/index.html')
+    return render(request, 'localsdirectory/index.html',({'tab':"home"}))
 
 
 # PRICES--------------------------------------------------------------------------------
@@ -62,12 +61,12 @@ def add_customer(request):
         else:
             print("Entries can't be empty")
         return redirect('index')
-    return render(request, 'add_customer.html')
+    return render(request, 'add_customer.html',({'tab':"customers"}))
 
 
 def view_customers(request):
     cust = customer.objects.all()
-    return render(request, 'view_customers.html', ({'customers': cust}))
+    return render(request, 'view_customers.html', ({'customers': cust,'tab':"customers"}))
 
 
 # ADVERTISEMENTS----------------------------------------------------------------------------
@@ -100,22 +99,24 @@ def add_advert(request):
         cust_id.append(c.cust_id)
     cust['name'] = cust_name
     cust['id'] = cust_id
-    return render(request, 'add_advert.html', (cust))
+    return render(request, 'add_advert.html', (cust),({'tab':"adv"}))
 
 
 def view_adverts(request):
     ads = adverts.objects.all()
-    return render(request, 'view_adverts.html', ({'advertisements': ads}))
+    return render(request, 'view_adverts.html', ({'advertisements': ads,'tab':"adv"}))
 
 
+
+#BILLING---------------------------------------------------------------------------------
 def billing(request):
     cust = customer.objects.filter()
     if request.method == 'POST':
         cust_id = request.POST.get('customer_id')
         ads = adverts.objects.filter(cust_id=cust_id, ad_status="Approved").order_by('ad_date_from')
         ads = ads.exclude(id__in=bills.objects.filter(cust_id=cust_id).values('ad_id'))
-        return render(request, 'billing.html', ({'cust': cust, 'ads': ads, 'customer_id': cust_id}))
-    return render(request, 'billing.html', ({'cust': cust}))
+        return render(request, 'billing.html', ({'cust': cust, 'ads': ads, 'customer_id': cust_id,'tab':"bills"}))
+    return render(request, 'billing.html', ({'cust': cust,'tab':"bills"}))
 
 
 def view_bills(request):
@@ -124,8 +125,8 @@ def view_bills(request):
         cust_id = request.POST.get('customer_id')
         bill = adverts.objects.raw("""Select * from mainactivity_adverts ad, mainactivity_bills bill where bill.ad_id=ad.id
          and ad.cust_id=%s""", [cust_id])
-        return render(request, 'view_bills.html', ({'bill': bill, 'cust': cust}))
-    return render(request, 'view_bills.html', ({'cust': cust}))
+        return render(request, 'view_bills.html', ({'bill': bill, 'cust': cust,'tab':"bills"}))
+    return render(request, 'view_bills.html', ({'cust': cust,'tab':"bills"}))
 
 
 def store_bills(request, cust_id):
@@ -159,12 +160,12 @@ def view_schedule(request):
                 'ad_date_from')
         else:
             schedule = adverts.objects.all().order_by('ad_date_from')
-    return render(request, 'view_schedule.html', ({'schedule': schedule}))
+    return render(request, 'view_schedule.html', ({'schedule': schedule,'tab':"schedule"}))
 
 
 def pending_for_approval(request):
     schedule = adverts.objects.filter(ad_status='Pending for approval').order_by('ad_date_from')
-    return render(request, 'pending_for_approval.html', ({'schedule': schedule}))
+    return render(request, 'pending_for_approval.html', ({'schedule': schedule,'tab':"adv"}))
 
 
 def accept(request, no):
@@ -176,7 +177,7 @@ def accept(request, no):
     ad = adverts.objects.filter(id=no)
     schedule = adverts.objects.filter(ad_status='Approved', ad_date_from__gte=ad[0].ad_date_from,
                                       ad_date_from__lte=ad[0].ad_date_till).order_by('ad_date_from')
-    return render(request, 'accept.html', ({'ad': ad, 'schedule': schedule}))
+    return render(request, 'accept.html', ({'ad': ad, 'schedule': schedule,'tab':"adv"}))
 
 
 def reject(request, no):
@@ -189,13 +190,13 @@ def reject(request, no):
         q.save()
         return redirect('pending_for_approval')
     ad = adverts.objects.filter(id=no)
-    return render(request, 'reject.html', ({'ad': ad}))
+    return render(request, 'reject.html', ({'ad': ad,'tab':"adv"}))
 
 
 def view_rejected(request):
     reject = rejected.objects.raw("""SELECT * FROM mainactivity_adverts, mainactivity_rejected where 
         mainactivity_adverts.id=mainactivity_rejected.ad_id order by mainactivity_rejected.rej_date desc""")
-    return render(request, 'view_rejected.html', ({'reject': reject}))
+    return render(request, 'view_rejected.html', ({'reject': reject,'tab':"adv"}))
 
 
 def pay_bills(request):
@@ -222,8 +223,8 @@ def pay_bills(request):
              and ad.cust_id=%s""", [cust_id])
         return render(request, 'pay_bills.html', ({'bill': bill, 'cust': cust, 'amount_due': amount_due,
                                                    'amount_paid': amount_paid, 'payment_date': payment_date,
-                                                   'payment_mode': payment_mode}))
-    return render(request, 'pay_bills.html', ({'cust': cust}))
+                                                   'payment_mode': payment_mode,'tab':"bills"}))
+    return render(request, 'pay_bills.html', ({'cust': cust,'tab':"bills"}))
 
 
 def pay_confirm(request, no):
@@ -282,4 +283,4 @@ def pay_confirm(request, no):
 def view_paid_bills(request):
     paid = rejected.objects.raw("""SELECT * FROM mainactivity_customer c, mainactivity_payments p where 
             c.cust_id=p.cust_id order by p.payment_date desc""")
-    return render(request, 'view_paid_bills.html', ({'paid': paid}))
+    return render(request, 'view_paid_bills.html', ({'paid': paid,'tab':"bills"}))
