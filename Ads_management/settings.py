1cf65b5a248c9eb1567b6ba5b2a884b6eae0ffc5
Manab <46393531+manabsaha@users.noreply.github.com>
2020-04-28 20:09:19 +0530

    minor

--- Ads_management/settings.py
+++ Ads_management/settings.py
@@ -1,8 +1,8 @@
 import os
 import django_heroku
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -109,12 +109,18 @@ USE_L10N = True
 
 USE_TZ = True
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
+STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + [‘static’]))
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 django_heroku.settings(locals())
