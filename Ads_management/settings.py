75f1751f184bab040150059fe7f579e85fbb974b
Manab <46393531+manabsaha@users.noreply.github.com>
2020-04-29 12:59:29 +0530

    h3

--- Ads_management/settings.py
+++ Ads_management/settings.py
@@ -138,3 +138,5 @@ STATICFILES_DIRS = [
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+del DATABASES['default']['OPTIONS']['sslmode']
