2cd550082944957e2bb68800692494e60026874d
Jaspreet8843 <Jaspreet8843@users.noreply.github.com>
2020-04-29 17:44:03 +0530

    navbar fixed

---
+++ Ads_management/local_settings.py
@@ -0,0 +1,13 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'janambhumi',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'3306',
+    }
+}
+
