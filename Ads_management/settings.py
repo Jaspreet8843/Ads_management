2cd91f0d2457172927b84085d3bdd2cd1ac137d7
Manab <46393531+manabsaha@users.noreply.github.com>
2020-04-29 07:49:15 +0530

    heroku

--- Ads_management/settings.py
+++ Ads_management/settings.py
@@ -74,13 +74,13 @@ WSGI_APPLICATION = 'Ads_management.wsgi.application'
 #     }
 # }
 
-DATABASES = { 'default' : dj_database_url.config()}
+#DATABASES = { 'default' : dj_database_url.config()}
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+    }
+}
 
 from django.db.backends.mysql.base import DatabaseWrapper
 DatabaseWrapper.data_types['DateTimeField'] = 'datetime' # fix for MySQL 5.5
